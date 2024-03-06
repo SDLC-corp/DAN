01c4aac06b361e2b025a12928644ad9a6da8dcd3
Harshal Mahajan <harshalmahajan@sdlccorp.com>
2024-03-06 16:17:29 +0530

    setting component updated

--- src/modules/settings/index.jsx
+++ src/modules/settings/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Breadcrumb, Button, Dropdown, Header, Icon, Input, Label, List, Menu, Modal, Popup, Search, Sidebar, Table } from 'semantic-ui-react';
+
 
 const Setting = () => {
+
+
+
+  const sections = [
+    { key: 'Dashboard', content: 'Dashboard', link: true },
+    { key: 'Documents List', content: 'Setting', active: true },
+  ];
+
+
   return (
     <div>
-       <h1>Settings</h1> 
+      {/* <div>
+        <Breadcrumb icon="right angle" sections={sections} />
+        <div className="header-text">All document list</div>
+        <div className="sub-text">List of all document List</div>
+      </div> */}
+      <div className="page-header" >
+            <div>
+              <Breadcrumb icon="right angle" sections={sections} />
+              <div className="header-text">Setting</div>
+              <div className="sub-text">List of all document List</div>
+            </div>
+          </div>
     </div>
   )
 }
